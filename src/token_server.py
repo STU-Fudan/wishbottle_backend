4c242ae3a309a3e3c2a9601c8a5557120230db3c
voidrank <voidrank@gmail.com>
2015-09-26 00:15:43 +0800

    fix bugs

--- src/token_server.py
+++ src/token_server.py
@@ -43,11 +43,25 @@ def get_signature():
         ticket_collection.drop()
         ticket_collection.insert({"content": ticket, "timestamp": timestamp})
 
-    signature = [url, str(timestamp), ticket, nonceStr]
-    signature = "".join(sorted(signature)).encode("utf-8")
+    signature = sorted([url, str(timestamp), ticket, nonceStr])
+    for index in range(len(signature)):
+        i = signature[index]
+        if i == url:
+            i = "url=" + url 
+        if i == str(timestamp):
+            i = "timestamp=" + str(timestamp)
+        if i == ticket:
+            i = "jsapi_ticket=" + ticket
+        if i == nonceStr:
+            i = "noncestr=" + nonceStr
+        if index > 0:
+            i = "&" + i
+        signature[index] = i
+    
+    signature = "".join(signature)
+    print(signature)
+
     signature = hashlib.sha1(signature).hexdigest()
-    signature = signature.encode("utf-8")
-    print(signature.__class__)
 
     return json.dumps({
         "timestamp": timestamp,
